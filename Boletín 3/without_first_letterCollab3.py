21d679dfd7f01403bf27b66cda0f34692c65e7ad
Roberto Matilla Augustinus <robertomatillaaugustinus@gmail.com>
2023-02-09 09:16:37 +0100

    Boletín 3

--- Boletín 3/without_first_letterCollab3.py
+++ Boletín 3/without_first_letterCollab3.py
@@ -1,1 +1,10 @@
 #Crea una función llamada without_first_letter que dada una lista de palabras, devuelva una nueva lista con la primera letra de cada palabra eliminada.
+
+def without_first_letter(lista):
+    lista2 = []
+    for i in lista:
+        lista2.append(i[1:])
+    return lista2
+
+lista = ["hola", "adios", "buenos dias"]
+print(without_first_letter(lista))
